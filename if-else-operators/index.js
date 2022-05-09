be833a04fc851b92038d80be02ff24b181d323c3
Your Name <ustinovoleksij@gmail.com>
2022-05-09 10:26:18 +0300

    Added ternary operators

---
+++ if-else-operators/index.js
@@ -0,0 +1,8 @@
+const askUser = prompt("Какое «официальное» название JavaScript?");
+
+if(askUser === "ECMAScript") {
+    console.log("Верно!")
+} else {
+    console.log("Не знаете? ECMAScript!")
+}
+
---
+++ if-else-operators/second.js
@@ -0,0 +1,9 @@
+const askNumber = +prompt("Enter the number");
+
+if(askNumber > 0) {
+    console.log(1)
+} else if (askNumber < 0){
+    console.log(-1)
+} else if(askNumber===0) {
+    console.log(0)
+}
