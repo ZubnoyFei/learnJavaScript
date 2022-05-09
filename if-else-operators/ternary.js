6b976a6964c8ebc6738478e20b7e9748f1185f32
Your Name <ustinovoleksij@gmail.com>
2022-05-09 10:41:18 +0300

    Added ternary operators

---
+++ if-else-operators/ternary.js
@@ -0,0 +1,15 @@
+
+// let a = +prompt("Enter the first number");
+// let b = +prompt("Enter the second number");
+//
+// let result= (a+b< 4) ? 'Мало': 'Много';
+
+
+// let message = prompt("Введите свою должность");
+//
+// let result = (message==='Сотрудник')?'Привет':
+//     (message==='Директор')?'Здравствуйте' :
+//         (message==='')?'Нет логина':
+//             '';
+//
+// console.log(result)
